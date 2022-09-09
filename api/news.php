2b9ee8964588ba2679209246dbf713986906becf
tung wei-i <ericliliwayne@gmail.com>
2022-09-09 23:44:45 +0800

    修改文章編號bug、完成問卷管理功能

--- api/news.php
+++ api/news.php
@@ -1,21 +1,13 @@
 <?php
     include_once "../base.php";
-    $_POST['del'];
-    $_POST['sh'];
-    if(isset($_POST['del'])){
-        foreach($_POST['del'] as $id){
-            $News->del($id);
-        }
-    }
-    $rows=$News->all();
-    foreach($rows as $row){
-        if(in_array($row['id'],$_POST['sh'])){
-            $row['sh']=1;
-        }else{
-            $row['sh']=0;
-        }
+   foreach($_POST['id'] as $id){
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        $News->del($id);
+    }else{
+        $row=$News->find($id);
+        $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
         $News->save($row);
     }
-
+   }
     to("../back.php?do=news");
 ?>
