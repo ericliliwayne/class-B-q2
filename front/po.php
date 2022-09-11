30c259b3a814d15719249698e70961169e30cc16
tung wei-i <ericliliwayne@gmail.com>
2022-09-11 23:58:41 +0800

    完成文章列表顯示

--- front/po.php
+++ front/po.php
@@ -10,22 +10,29 @@
 </style>
 <div>目前位置 : 首頁 > 分類網誌 > <span id="header">健康新知</span></div>
 <div style="display: flex;">
-    <fieldset>
+    <fieldset style="width: 15%;">
         <legend>分類網誌</legend>
         <div class="type">健康新知</div>
         <div class="type">菸害防制</div>
         <div class="type">癌症防治</div>
         <div class="type">慢性病防治</div>
     </fieldset>
-    <fieldset>
+    <fieldset style="width: 85%;">
         <legend>文章列表</legend>
-        <div></div>
+        <div id="content"></div>
     </fieldset>
 </div>
 
 <script>
+    getList("健康新知");
     $(".type").on("click",function(){
         let type = $(this).text();
         $("#header").html(type);
+        getList(type);
     })
+    function getList(type){
+        $.get("./api/get_list.php",{type},(list)=>{
+            $("#content").html(list);
+        })
+    }
 </script>
